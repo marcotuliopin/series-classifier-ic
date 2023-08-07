0f4301bf75e40b0127b28417ff6d6a001f769c51
marcotuliopin <marcotuliopin@gmail.com>
2023-08-06 21:44:36 -0300

    first commit

---
+++ export_creation.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+
+def plot_distribution(histogram, DATA_NAME, class_name, n, window_size, n_bins):
+    """
+    Plot histogram of the distribution of the data across all possible symbols present in it.
+    """
+    sns.set_theme(style='whitegrid')
+    sns.set_context("paper", rc={"font.size":20,"axes.titlesize":20,"axes.labelsize":20})  
+    bin_count = int(np.ceil(np.log2(len(histogram))) + 1)
+    f, ax = plt.subplots(figsize=(15, 8))
+    fig = histogram.plot.hist(bins=bin_count)
+    sns.set(font_scale=1.0)
+    fig.bar_label(fig.containers[0])
+    fig.set(title=f'Symbol\'s Appearence in {DATA_NAME} - class = {str(class_name)} - instance = {str(n)} - window size = {window_size} - n_bins = {n_bins}', ylabel='Appearence Count', xlabel='Symbol')
+    # save figure
+    Path(f"../fig/distribution/{DATA_NAME}/window_{window_size}/bins_{n_bins}/class_{str(class_name)}").mkdir(parents=True, exist_ok=True)
+    plt.savefig(f'../fig/distribution/{DATA_NAME}/window_{window_size}/bins_{n_bins}/class_{str(class_name)}/fig{str(n)}.png')
+    plt.close()
+
+
+def save_js_metrics(eq_class, diff_class, DATA_NAME, window_size, n_bins):
+    """
+    Save the results of the Jensenshannon distance calculation to a CSV and a TXT file.
+    """
+    Path(f'../metrics/{DATA_NAME}/window_{window_size}/bins_{n_bins}').mkdir(parents=True, exist_ok=True)
+
+    # plot js difference
+    plot_js_box(eq_class, diff_class, DATA_NAME, window_size, n_bins)
+
+    with open(f'../metrics/{DATA_NAME}/window_{window_size}/bins_{n_bins}/js.txt', 'w') as file:
+        same_class_mean = np.mean(eq_class)
+        same_class_median = np.median(eq_class)
+        diff_class_mean = np.mean(diff_class)
+        diff_class_median = np.median(diff_class)
+        difference = (1 - same_class_mean/diff_class_mean) * 100
+        file.write('Same class JS - Mean: {}; Median: {}\n\n'.format(same_class_mean, same_class_median))
+        file.write('Different classes JS - Mean: {}; Median: {}\n\n'.format(diff_class_mean, diff_class_median))
+        file.write('Percentual difference between same class and different class: {}'.format(difference))
+
+
+def plot_js_box(eq_class, diff_class, DATA_NAME, window_size, n_bins):
+    """
+    Plot boxplot of Jensen Shannon distance between samples of different classes and of the same class.
+    """
+    sns.set_theme(style='whitegrid')
+    sns.set_context("paper", rc={"font.size":15,"axes.titlesize":15,"axes.labelsize":15})  
+    plt.figure(figsize=(8, 6))
+
+    eq_class = eq_class.apply(pd.Series).stack().reset_index(drop=True)
+    diff_class = diff_class.apply(pd.Series).stack().reset_index(drop=True)
+    combined_distances = pd.DataFrame({'Equal Class' : eq_class,
+                                       'Different Class' : diff_class})
+
+    fig = sns.boxplot(data=combined_distances, orient='h')
+    fig.set_xlabel(f'Distance')
+    Path(f'../fig/js/{DATA_NAME}/window_{window_size}/bins_{n_bins}').mkdir(parents=True, exist_ok=True)
+    plt.savefig(f'../fig/js/{DATA_NAME}/window_{window_size}/bins_{n_bins}/fig.png')
+    plt.close()
+
+
+def plot_entropy_sc(comp_entrop, labels, DATA_NAME, window_size, n_bins):
+    """
+    Plot scatterplot of entropy x statistical complexity of the data.
+    """
+    sns.set_theme(style='whitegrid')
+    sns.set_context("paper", rc={"font.size":15,"axes.titlesize":15,"axes.labelsize":15})  
+    classes = np.unique(labels)
+
+    plt.figure(figsize=(8, 6))
+    for i in range(len(classes)):
+        indices = labels == classes[i]
+        plt.scatter(comp_entrop.loc[indices, 'entropy'], comp_entrop.loc[indices, 'statistical_complexity'], label=classes[i], alpha=0.5, s=60)
+
+    plt.xlabel('Entropy')
+    plt.title(f'{DATA_NAME} - window size = {window_size} - n_bins = {n_bins}')
+    plt.ylabel('Statistical Complexity')
+    plt.legend()
+    #save figure
+    Path(f"../fig/entropy_sc/{DATA_NAME}/window_{window_size}/bins_{n_bins}").mkdir(parents=True, exist_ok=True)
+    plt.savefig(f'../fig/entropy_sc/{DATA_NAME}/window_{window_size}/bins_{n_bins}/fig.png')
+    plt.close()
---
+++ functions.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import numpy as np
+import itertools
+from pyts.approximation import SymbolicAggregateApproximation
+from sklearn.preprocessing import StandardScaler
+from scipy.spatial.distance import jensenshannon
+
+
+def run_sax(data, n_bins): 
+    """
+    Calculate SAX transformation
+
+    -----------
+    Parameters:
+    data (pd.Series): Series containing the data to use in the transformation.
+    n_bins (int): The number of letters to use in the Symbolic Aggregate Approximation (SAX) representation of the data. 
+
+    -----------
+    Returns:
+    sax_values (np.array of string): SAX sequence.
+    """
+
+    scaler = StandardScaler()
+    scaled_data = scaler.fit_transform(data.values.reshape(-1, 1))
+    sax_model = SymbolicAggregateApproximation(n_bins=n_bins,strategy='normal')
+    sax_values = sax_model.fit_transform(scaled_data.reshape(1, -1))
+    return sax_values[0]
+
+
+def create_new_representation(data, window_size, dict):
+    """
+    Group symbols in the pd.Series and create a new representation of lower dimensionality of the data
+
+    -----------
+    Parameters:
+    data (pd.Series): Series containing the data to use in the transformation.
+    window_size (int): The number of symbols to be groupped at a time.
+    dict (dictionary): The encoding of each symbol to a numeric value.
+
+    -----------
+    Returns:
+    new_representation (pd.Series): new data representation.
+    """
+
+    new_representation = []
+    for i in range(0, len(data) - window_size + 1):
+        interval = slice(i, i + window_size)
+        data_window = tuple(data[interval])
+        new_representation.append(dict[data_window])
+    return pd.Series(new_representation)
+
+
+def calculate_js_distance(data, labels):
+    """
+    Calculate the mean Jensenshannon distance between pairs of points for each pair of classes.
+
+    -----------
+    Parameters:
+    data (pd.DataFrame): Series containing the data to use in the transformation.
+    labels (pd.Series): labels of the dataset.
+
+    -----------
+    Returns:
+    js_mean (pd.DataFrame): Jensen Shannon distance between samples of distinct or same class.
+    """
+    symbols = np.unique(np.unique(s) for s in data)
+    classes = np.unique(labels)
+    print('ok')
+    js = []
+    # do for each pair of classes
+    for c1 in classes:
+        for c2 in classes:
+            js_distance = []
+            # get data from each class
+            class_data_1 = data[labels == c1]
+            class_data_2 = data[labels == c2]
+            # calculate jensenshannon distance for each pair of points
+            for i in range(len(class_data_1)):
+                frequency_1 = calculate_frequency(class_data_1[i], symbols)
+                for j in range(len(class_data_2)):
+                    frequency_2 = calculate_frequency(class_data_2[j], symbols)
+                    js_distance.append(jensenshannon(frequency_1, frequency_2))
+            js.append([c1, c2, js_distance])
+            print(js[-1])
+    js = pd.DataFrame(data=js, columns=['Class 1', 'Class 2', 'JS Distance'])
+    return js
+
+
+def calculate_frequency(data, symbols):
+    """
+    Calculate the frequency of each symbol in a dataset.
+    """
+    unique, counts = np.unique(data, return_counts=True)
+    counts = counts / counts.sum()
+    frequency = []
+    for symbol in symbols:
+        if symbol in unique:
+            frequency.append(counts[np.where(unique == symbol)[0][0]])
+        else:
+            frequency.append(0)
+    return np.array(frequency)
+
+
+def compute_symbols_dictionary(sax_unique_values, window_size):
+    """
+    Compute the encoding of each symbol to a numeric value.
+    
+    -----------
+    Parameters:
+    sax_unique_values
+    window_size
+
+    -----------
+    Returns:
+    dict (dictionary): The encoding of each symbol to a numeric value.
+    """
+    l = []
+    for i in range(window_size + 1):
+        for combination in itertools.product(sax_unique_values, repeat=i):
+            l.append(combination)
+    l.sort()
+    l.remove(())
+    dict = {}
+    for i in range(len(l)):
+        dict[l[i]] = i
+    return dict
+
+
+def compute_transition_matrix(data, n_symbols):
+    """
+    Compute transition matrix
+
+    -----------
+    Parameters:
+    data (pd.Series): Series containing the data to use in the transition matrix.
+    n_symbols (int): The number of unique symbols in the data. 
+
+    -----------
+    Returns:
+    transition_matrix (pd.Dataframe of float): Markov transition matrix.
+    """
+    transition_matrix = pd.DataFrame(np.zeros((n_symbols, n_symbols)), columns=np.unique(data), index=np.unique(data))
+    for i in range(len(data) - 1):
+        transition_matrix[data[i]][data[i + 1]] += 1
+    return transition_matrix
